13ec11733a274209f61eb853827cef1f9703e8c1
Sofio Katamadze <sofio.katamadze@coxautoinc.com>
2025-06-16 18:48:08 +0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+def caesar_decrypt(cipher_text, shift):
+    decrypted_text = ""
+    for char in cipher_text:
+        if char.isalpha():
+            offset = 65 if char.isupper() else 97
+            decrypted_text += chr((ord(char) - offset - shift) % 26 + offset)
+        else:
+            decrypted_text += char
+    return decrypted_text
+
+cipher_text = "Hvs Eiwqy Pfckb Tcl Xiadg Cjsf Hvs Zonm Rcu."
+
+for shift in range(1, 26):
+    decrypted = caesar_decrypt(cipher_text, shift)
+    print(f"Shift {shift}: {decrypted}")
